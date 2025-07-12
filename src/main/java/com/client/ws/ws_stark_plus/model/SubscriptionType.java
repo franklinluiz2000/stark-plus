f9f84edbf3a44833e9baebb16fb7c5e7d3983243
unknown <franklinluiz2000@gmail.com>
2025-07-12 10:17:48 -0300

    Implementado a notação do lombok

--- src/main/java/com/client/ws/ws_stark_plus/model/SubscriptionType.java
+++ src/main/java/com/client/ws/ws_stark_plus/model/SubscriptionType.java
@@ -9,7 +9,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name = "subscriptions_type")
 @Entity
 public class SubscriptionType implements Serializable {
@@ -31,54 +39,4 @@ public class SubscriptionType implements Serializable {
     @Column(name = "product_key")
     private String productKey;
 
-    public SubscriptionType(Long id, String name, Long accessMonth, BigDecimal price, String productKey) {
-	this.id = id;
-	this.name = name;
-	this.accessMonth = accessMonth;
-	this.price = price;
-	this.productKey = productKey;
-    }
-
-    public SubscriptionType() {
-    }
-
-    public Long getId() {
-	return id;
-    }
-
-    public void setId(Long id) {
-	this.id = id;
-    }
-
-    public String getName() {
-	return name;
-    }
-
-    public void setName(String name) {
-	this.name = name;
-    }
-
-    public Long getAccessMonth() {
-	return accessMonth;
-    }
-
-    public void setAccessMonth(Long accessMonth) {
-	this.accessMonth = accessMonth;
-    }
-
-    public BigDecimal getPrice() {
-	return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-	this.price = price;
-    }
-
-    public String getProductKey() {
-	return productKey;
-    }
-
-    public void setProductKey(String productKey) {
-	this.productKey = productKey;
-    }
 }
--- src/main/java/com/client/ws/ws_stark_plus/model/UserType.java
+++ src/main/java/com/client/ws/ws_stark_plus/model/UserType.java
@@ -8,7 +8,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "user_type")
 public class UserType implements Serializable {
@@ -24,36 +32,4 @@ public class UserType implements Serializable {
 
     private String description;
 
-    public UserType(Long id, String name, String description) {
-	this.id = id;
-	this.name = name;
-	this.description = description;
-    }
-
-    public UserType() {
-    }
-
-    public Long getId() {
-	return id;
-    }
-
-    public void setId(Long id) {
-	this.id = id;
-    }
-
-    public String getName() {
-	return name;
-    }
-
-    public void setName(String name) {
-	this.name = name;
-    }
-
-    public String getDescription() {
-	return description;
-    }
-
-    public void setDescription(String description) {
-	this.description = description;
-    }
 }
